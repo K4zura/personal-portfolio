2f244d492f07b69cd092453c26b1de697007dfb5
k4zura <johannbb0520@gmail.com>
2025-08-20 13:49:21 -0500

    add icons for technologies

---
+++ src/assets/icons/Vercel.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let className = "";
+</script>
+
+<svg
+  class={className}
+  viewBox="0 0 256 222"
+  width="256"
+  height="222"
+  xmlns="http://www.w3.org/2000/svg"
+  preserveAspectRatio="xMidYMid"
+  ><path class="fill-text-primary" d="m128 0 128 221.705H0z" /></svg
+>
